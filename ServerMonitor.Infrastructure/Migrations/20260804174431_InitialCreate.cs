7c28896547d6988dc50eef1084d83d5ea0160b47
SoftGene <bagmala80@gmail.com>
2026-08-04 20:20:53 +0200

    Add project files.

---
+++ ServerMonitor.Infrastructure/Migrations/20260804174431_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ServerMonitor.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MetricSnapshots",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TimestampUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CpuUsagePercent = table.Column<double>(type: "double precision", nullable: false),
+                    MemoryUsedMb = table.Column<double>(type: "double precision", nullable: false),
+                    MemoryTotalMb = table.Column<double>(type: "double precision", nullable: false),
+                    DiskUsedGb = table.Column<double>(type: "double precision", nullable: false),
+                    DiskTotalGb = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MetricSnapshots", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MetricSnapshots");
+        }
+    }
+}
---
+++ SeverMonitor.Domain/Entities/MetricSnapshot.cs
@@ -0,0 +1,18 @@
+﻿namespace ServerMonitor.Domain.Entities;
+
+public class MetricSnapshot
+{
+    public int Id { get; set; }
+
+    public DateTime TimestampUtc { get; set; }
+
+    public double CpuUsagePercent { get; set; }
+
+    public double MemoryUsedMb { get; set; }
+
+    public double MemoryTotalMb { get; set; }
+
+    public double DiskUsedGb { get; set; }
+
+    public double DiskTotalGb { get; set; }
+}
